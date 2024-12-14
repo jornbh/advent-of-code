6497be1a68bd3857a2858c30c28b29f957b95493
Jørn Bøni Hofstad <jorn496@gmail.com>
2024-12-14 20:27:58 +0100

    not able to finish

---
+++ 2024/13/part-1/doit.py
@@ -0,0 +1,6 @@
+
+
+# We can take one line from each
+# Make a linear expression
+# See if the solution is solvable, and if it has any negative values
+# Also see if it has decimal values, if not, it can not solve anything
