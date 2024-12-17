37f8d37ec24afad302c925c3baeff89674d4b5bc
Jørn Bøni Hofstad <jorn.hofstad@nordicsemi.no>
2024-12-17 11:50:32 +0100

    did part 1 now for part 2

--- 2024/12/part-1/doit.py
+++ 2024/12/part-1/doit.py
@@ -23,20 +23,68 @@ def lmatget(point, mat=lmat) :
     return mat[x][y]
 
 
-type_sets = map(set, lmat)
-types = set.union(*type_sets) - {None}
-char_to_num = {char: ind for ind, char in enumerate(types)}
-print("char_to_num  = " , char_to_num )
-print("types  = " , types )
+def discover_frontier(point, discovered = None , mat=lmat):
+    if discovered is None:
+        discovered = {point}
+
+    current_val = lmatget(point, mat=mat)
+
+    same_border = { new_point for new_point in border(point) if lmatget(new_point) == current_val}
+    frontier = same_border   - discovered
+
+    discovered.update(frontier)
+
+    # Visit ing the different points will update the discovered set
+    for new_point in frontier:
+        discover_frontier(new_point, discovered=discovered, mat=mat)
+
+    assert len(discovered) > 0
+    return discovered
+
+
+
+
+
+#type_sets = map(set, lmat)
+#types = set.union(*type_sets) - {None}
+#char_to_num = {char: ind for ind, char in enumerate(types)}
+#print("char_to_num  = " , char_to_num )
+#print("types  = " , types )
 
 
 matching_numbers = np.zeros([len(lmat) -1, len(lmat[0])-1], np.int64)
+areas = np.zeros([len(lmat) -1, len(lmat[0])-1], np.int64)
 
-# DOn't care about the None padding
+remembered = dict()
+
+# Don't care about the None padding
 for lind, line in enumerate(lmat[:-1]):
     for cind, char in enumerate(line[:-1]):
         for tel in border((lind, cind)):
             if char == lmatget(tel, mat=lmat):
                 matching_numbers[lind, cind] += 1
 
-print("matching_numbers  = " , matching_numbers )
+        # Do the proper border thing per element
+        if remembered.get((lind, cind)) is None:
+            frontier = discover_frontier((lind,cind))
+
+            area = len(frontier )
+            for p in frontier:
+                remembered[p] = area
+        else:
+            area = remembered[(lind, cind)]
+        assert area > 0
+        areas[lind, cind]= area
+
+print("matching_numbers  = \n" , matching_numbers )
+print("areas  = \n" , areas )
+fences = 4 - matching_numbers
+print("fences  = \n" , fences )
+
+tot = np.sum(areas*(fences))
+print("tot  = \n" , tot )
+#disc = discover_frontier((0,0))
+#disc_list = list(disc)
+#disc_list.sort()
+#print("disc_list  = " , disc_list )
+#print("matching_numbers  = " , matching_numbers )
